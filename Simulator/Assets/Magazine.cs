c84adc53acf9a509f2b9bfabbfa688961be04c07
Freezie7 <vanyazhem777@gmail.com>
2022-03-20 12:46:54 +0300

    Добавил проект!!!!

---
+++ Simulator/Assets/Magazine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magazine : MonoBehaviour {
+    public GameObject Menu;
+    public GameObject Magaz;
+   public AudioSource audio;
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+	}
+    private void OnMouseDown()
+    {
+        Menu.SetActive(false);
+        Magaz.SetActive(true);
+     
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    void primer()
+    {
+
+    }
+}
